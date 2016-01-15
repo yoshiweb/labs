0a22c3776046ac5b5ed7fb0e2ad199e3c392d33e
yoshiweb <yoshiweb@gmail.com>
2016-01-15 20:10:28 +0900

    cssでブラーをかける

---
+++ html/threejs/src/006/README.md
@@ -0,0 +1,11 @@
+# three.js 実験
+
+http://yoshiweb.github.io/labs/html/threejs/
+
+* 何もない世界に立方体を配置（回転）
+* OrbitControls 追加
+* AxisHelper 追加
+* 地面を配置
+* three.jsの背景を透過させる
+* cssブレンドモードで合成
+* css ブラー
---
+++ html/threejs/src/006/js/main.js
@@ -0,0 +1,84 @@
+(function () { 
+    "use strict";
+
+    /* シーン作成 */
+    var scene = new THREE.Scene();
+
+
+    /* 物体を生成 */
+
+    // 立方体
+    var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+    var material = new THREE.MeshLambertMaterial( { color: 0x00ff00 } );
+    var cube = new THREE.Mesh( geometry, material );
+    scene.add( cube );
+
+    /* 光を作成 */
+    var light    = new THREE.DirectionalLight(0xffffff, 1.5); // 光源の色, 強さ
+    light.position.set(1,1,1);// 光源の位置
+    scene.add(light);// シーンに追加
+
+
+    // 平面
+    var geometry = new THREE.PlaneGeometry( 1000, 1000, 10, 10 );
+    var material = new THREE.MeshBasicMaterial({
+        wireframe:true
+    });
+    var _plane = new THREE.Mesh( geometry, material );
+    scene.add(_plane);
+
+    // 平面を倒す
+    _plane.rotation.x = Math.PI * 0.5;
+    _plane.position.y = -100;
+
+
+
+
+
+
+
+
+    /* カメラ作成 */
+    var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+    // カメラ位置を設定
+    camera.position.z = 5;
+
+
+    // カメラオブジェクトを引数に生成
+    var controls = new THREE.OrbitControls(camera);
+
+
+    // AxisHelper
+    var axisHelper = new THREE.AxisHelper( 5 );
+    scene.add( axisHelper );
+
+
+
+
+    /* レンダラー作成 */
+    var renderer = new THREE.WebGLRenderer({antialias: true, alpha: true});// 背景透過
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.setClearColor(0xcccccc, 0);// 色、透明度
+    renderer.autoClear = false;
+
+    // canvas要素に配置
+    var canvas = document.getElementById("canvas");
+    canvas.appendChild(renderer.domElement);
+
+    /* レンダリング */
+    var render = function () {
+        // 繰り返し実行
+        requestAnimationFrame( render );
+
+        // 物体を回転
+        cube.rotation.x += 0.01;
+        cube.rotation.y += 0.01;
+
+        // カメラで撮影したシーンを描画
+        renderer.render(scene, camera);
+    };
+
+    // 実行
+    render();
+
+})();
